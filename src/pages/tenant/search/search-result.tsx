2de8325cd45e3c77a83d1254a23d61458f99307b
ryanfavour4 <ryanfave022@gmail.com>
2025-04-28 16:38:07 +0100

    Refactor code structure for improved readability and maintainability

--- src/pages/tenant/search/search-result.tsx
+++ src/pages/tenant/search/search-result.tsx
@@ -3,6 +3,8 @@ import SideNavbar from "@/layout/side-navbar";
 import TopNavbar from "@/layout/top-navbar";
 import { useNavigate } from "react-router-dom";
 import listingHouse1 from "@/assets/images/listing-house-1.jpg";
+import loaderGif from "@/assets/images/loader.gif";
+import nothingFoundGif from "@/assets/images/nothing-found.gif";
 
 export default function SearchResult() {
     return (
@@ -44,17 +46,36 @@ export default function SearchResult() {
 
 export function SearchResultMainContent() {
     return (
-        <div className="px-4 bg-light-100 py-4">
-            <div className="pb-10">
-                {/* Section 1 */}
-                <p className="text-lg font-semibold mb-2">Search Result:</p>
+        <>
+            <div className="px-4 bg-light-100 py-4">
+                <div className="pb-10">
+                    {/* Section 1 */}
+                    <p className="text-lg font-semibold mb-2">Search Result:</p>
 
-                <div className="py-3 rounded-md flex flex-col mt-2 gap-5">
-                    <HouseSearchResultCard />
-                    <HouseSearchResultCard />
+                    <div className="py-3 rounded-md flex flex-col mt-2 gap-5">
+                        <HouseSearchResultCard />
+                        <HouseSearchResultCard />
+                    </div>
                 </div>
             </div>
-        </div>
+            <div className="hidden inset-0 flex-1 items-center justify-center fixed bg-white">
+                <img
+                    src={loaderGif}
+                    className="size-56 rounded-lg object-cover"
+                />
+            </div>
+            <div className="flex flex-1 items-center justify-center flex-col">
+                <img
+                    src={nothingFoundGif}
+                    className="size-56 rounded-lg object-cover"
+                />
+
+                <p className="text-center">
+                    Sorry, no property fits {`\n`} into your search {`\n`}
+                    request at this time...
+                </p>
+            </div>
+        </>
     );
 }
 
